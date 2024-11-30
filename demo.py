3b6252d3aeab0e62785b1db6767cd53bd84d60ae
MichaelE11 <electmike84@gmail.com>
2024-11-30 23:08:30 +0100

    ingestion done

--- demo.py
+++ demo.py
@@ -1,10 +1,3 @@
-from US_Visa.logger import logging
-import sys
-from US_Visa.exception import USvisaException
-
-logging.info("welcome to our custom loggingh")
-
-try:
-    2/0
-except Exception as e:
-    raise USvisaException(e,sys) from e
+from US_Visa.pipeline.training_pipeline import TrainPipeline
+obj=TrainPipeline()
+obj.run_pipeline()
